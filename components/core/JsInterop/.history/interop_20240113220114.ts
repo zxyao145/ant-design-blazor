aa72d2a03db94fe8cf7be731bbb16bef18e64142
zxyao <zxyao145@gmail.com>
2024-01-13 22:43:40 +0800

    fix: modal dom instance clear when url changed

---
+++ components/core/JsInterop/.history/interop_20240113220114.ts
@@ -0,0 +1,39 @@
+export { state } from './modules/stateProvider';
+export * as observable from './ObservableApi/observableApi';
+export { domInfoHelper, domTypes, domManipulationHelper, eventHelper } from './modules/dom/exports';
+export { styleHelper } from './modules/styleHelper';
+export {
+    backtopHelper,
+    iconHelper,
+    imageHelper,
+    inputHelper,
+    mentionsHelper,
+    modalHelper,
+    overlayHelper,
+    tableHelper,
+    uploadHelper,
+    downloadHelper,
+    watermarkHelper,
+} from './modules/components/export'
+
+
+export { enableDraggable, disableDraggable, resetModalPosition } from "./modules/dom/dragHelper";
+
+export { generate as generateColor } from "@ant-design/colors";
+
+import {modalHelper} from './modules/components/export'
+import {manipulationHelper}from './modules/dom/manipulationHelper'
+
+export function log(text) {
+    console.log(text);
+}
+
+export function onLocationChanged() {
+    modalHelper.destroyAllDialog();
+    manipulationHelper.enableBodyScroll(true)
+    document.body
+    document.querySelectorAll('.ant-modal-root')
+    .forEach(e => document.body.removeChild(e.parentNode));
+    document.querySelectorAll('.ant-modal-root')
+    .forEach(e => document.body.removeChild(e.parentNode));
+}
---
+++ components/core/JsInterop/.history/interop_20240113220207.ts
@@ -0,0 +1,35 @@
+export { state } from './modules/stateProvider';
+export * as observable from './ObservableApi/observableApi';
+export { domInfoHelper, domTypes, domManipulationHelper, eventHelper } from './modules/dom/exports';
+export { styleHelper } from './modules/styleHelper';
+export {
+    backtopHelper,
+    iconHelper,
+    imageHelper,
+    inputHelper,
+    mentionsHelper,
+    modalHelper,
+    overlayHelper,
+    tableHelper,
+    uploadHelper,
+    downloadHelper,
+    watermarkHelper,
+} from './modules/components/export'
+
+
+export { enableDraggable, disableDraggable, resetModalPosition } from "./modules/dom/dragHelper";
+
+export { generate as generateColor } from "@ant-design/colors";
+
+import {modalHelper} from './modules/components/export'
+import {manipulationHelper}from './modules/dom/manipulationHelper'
+
+export function log(text) {
+    console.log(text);
+}
+
+export function onLocationChanged() {
+    modalHelper.destroyAllDialog();
+    manipulationHelper.enableBodyScroll(true)
+    document.querySelectorAll('.ant-drawer').forEach(e => document.body.removeChild(e.parentNode));
+}
